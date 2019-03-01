41b5d5ad8c7e0946e1c8e21804d100e7e8cc2e1a
Lucas Nolting <lnolting22@gmail.com>
2019-02-28 23:48:41 -0600

    I finished a lot of this, some functionality and all styling is still required

--- app.js
+++ app.js
@@ -1,29 +1,90 @@
 //Vue Components
-Vue.component('line-item',{
-    props:['item'],
-    template: '<li>{{item.id}} {{item.name}} {{item.quantity}} {{item.isFavorite}}</li>'
-});
+Vue.component('line-item', {
+    props: ['item'],
+    methods: {
+        toggleFavorite: function (event) {
+            if (this.item.isFavorite) {
+                this.item.isFavorite = false;
+            } else {
+                this.item.isFavorite = true;
+            }
+        },
 
-Vue.component('favorite-button',{
-    props:['item'],
-    methods:{
-        toggleFav: function () {
-            
+        deleteItem: function (event) {
+            if (confirm("Are you sure?")) {
+                Vue.delete(this.item);
+            } else {
+                //do nothing
+            }
         }
     },
-    template:'<button v-on:toggle-fav>favorite</button>'
+    template: '<li>{{item.name}} {{item.quantity}} {{item.isFavorite}} ' +
+    '<button v-on:click="toggleFavorite">Favorite</button>' +
+    '<button v-on:click="$emit(\'remove\')">Delete</button></li>'
 });
-
 
 new Vue({
 
-    el:"#app",
+    el: "#app",
     data: {
         items: [
-            { id: 1, name: 'An item', quantity: 7, isFavorite: false},
-            { id: 1, name: 'Number two', quantity: 1, isFavorite: true},
-            { id: 1, name: 'Three', quantity: 3, isFavorite: false}
-        ]
+            {id: 1, name: 'An item', quantity: 7, isFavorite: false},
+            {id: 2, name: 'Number two', quantity: 1, isFavorite: true},
+            {id: 3, name: 'Three', quantity: 3, isFavorite: false}
+        ],
+
+        tempName: " ",
+        tempQuantity: 0,
+
+        displayOnlyFavorites: false
+    },
+
+    methods: {
+        addItem() {
+
+
+            for (let i = 0; i < this.items.length; ++i) {
+
+                if (this.items[i].name.trim() === this.tempName.trim()) {
+                    this.items[i].quantity = parseInt(this.tempQuantity) + parseInt(this.items[i].quantity);
+                    break;
+                }
+
+
+
+                if (i === (this.items.length - 1)) {
+                    let unusedId = i + 2;
+                    let makeSureIsInt = this.tempQuantity;
+                    let newItem = {
+                        id: unusedId,
+                        name: this.tempName,
+                        quantity: makeSureIsInt,
+                        isFavorite: false
+                    };
+
+                    this.items.push(newItem);
+                    break;
+                }
+
+            }
+
+            if(this.items.length === 0) {
+                let newItem = {
+                    id: 0,
+                    name: this.tempName,
+                    quantity: this.quantity,
+                    isFavorite:false
+                };
+
+                this.items.push(newItem)
+            }
+        },
+    },
+
+    computed: {
+        filteredItems() {
+            return this.items.filter(item => {return item.isFavorite === true})
+        }
     }
 });
 
