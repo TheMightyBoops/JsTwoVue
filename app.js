5c8fe8dae744597b40aa5dcb40a947b2a7df094d
Lucas Nolting <lnolting22@gmail.com>
2019-03-03 15:42:38 -0600

    finished everything, didn't commit much

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
+
 //Vue Components
 Vue.component('line-item', {
-    props: ['item'],
+    props: {item:Object, nameOfItemToEdit:String},
     methods: {
         toggleFavorite: function (event) {
             if (this.item.isFavorite) {
@@ -10,17 +12,17 @@ Vue.component('line-item', {
             }
         },
 
-        deleteItem: function (event) {
-            if (confirm("Are you sure?")) {
-                Vue.delete(this.item);
-            } else {
-                //do nothing
-            }
+        editThisItemByName: function(event) {
+          this.$parent.$parent.$parent.$parent.nameOfItemToEdit = this.item.name;
         }
-    },
-    template: '<li>{{item.name}} {{item.quantity}} {{item.isFavorite}} ' +
-    '<button v-on:click="toggleFavorite">Favorite</button>' +
-    '<button v-on:click="$emit(\'remove\')">Delete</button></li>'
+    }
+
+    ,
+    template: '<v-list><li>{{item.name}}<i class=material-icons v-if="item.isFavorite">favorite</i></li> ' +
+    '<li><strong>Qty:</strong>{{item.quantity}}</li>' +
+    '<v-btn color="success" v-on:click="toggleFavorite">Favorite</v-btn>' +
+    '<v-btn color="warning" v-on:click="editThisItemByName">edit</v-btn>'+
+    '<v-btn color="error" v-on:click="$emit(\'remove\')">Delete</v-btn></v-list>'
 });
 
 new Vue({
@@ -28,57 +30,76 @@ new Vue({
     el: "#app",
     data: {
         items: [
-            {id: 1, name: 'An item', quantity: 7, isFavorite: false},
-            {id: 2, name: 'Number two', quantity: 1, isFavorite: true},
-            {id: 3, name: 'Three', quantity: 3, isFavorite: false}
+            {id: 1, name: 'Furbies', quantity:20000 , isFavorite: true},
+            {id: 2, name: 'Tonnes of AAA batteries', quantity: 100, isFavorite: true},
+            {id: 3, name: 'Furby Sized Rail Guns', quantity: 200000, isFavorite: false},
+            {id: 4, name: 'Ai-chips', quantity:200000, isFavorite:false}
         ],
 
         tempName: " ",
         tempQuantity: 0,
 
-        displayOnlyFavorites: false
+        displayOnlyFavorites: false,
+
+        nameOfItemToEdit: " ",
+        nameToChangeItTo: " ",
+        quantityToChangeItTo: " "
     },
 
     methods: {
         addItem() {
 
+            if(this.tempName.trim() !== "") {
+                for (let i = 0; i < this.items.length; ++i) {
 
-            for (let i = 0; i < this.items.length; ++i) {
+                    if (this.items[i].name.trim() === this.tempName.trim()) {
+                        this.items[i].quantity = parseInt(this.tempQuantity) + parseInt(this.items[i].quantity);
+                        break;
+                    }
 
-                if (this.items[i].name.trim() === this.tempName.trim()) {
-                    this.items[i].quantity = parseInt(this.tempQuantity) + parseInt(this.items[i].quantity);
-                    break;
+
+                    if (i === (this.items.length - 1)) {
+                        let unusedId = i + 2;
+                        let makeSureIsInt = this.tempQuantity;
+                        let newItem = {
+                            id: unusedId,
+                            name: this.tempName,
+                            quantity: makeSureIsInt,
+                            isFavorite: false
+                        };
+
+                        this.items.push(newItem);
+                        break;
+                    }
+
                 }
 
-
-
-                if (i === (this.items.length - 1)) {
-                    let unusedId = i + 2;
-                    let makeSureIsInt = this.tempQuantity;
+                if (this.items.length === 0) {
                     let newItem = {
-                        id: unusedId,
+                        id: 0,
                         name: this.tempName,
-                        quantity: makeSureIsInt,
+                        quantity: this.quantity,
                         isFavorite: false
                     };
 
-                    this.items.push(newItem);
-                    break;
+                    this.items.push(newItem)
                 }
-
-            }
-
-            if(this.items.length === 0) {
-                let newItem = {
-                    id: 0,
-                    name: this.tempName,
-                    quantity: this.quantity,
-                    isFavorite:false
-                };
-
-                this.items.push(newItem)
             }
         },
+
+        editItem() {
+            for(let i=0; i < this.items.length; ++i) {
+                if(this.nameOfItemToEdit.trim() === this.items[i].name.trim()) {
+                    if(this.nameToChangeItTo.trim() !== '') {
+                        this.items[i].name = this.nameToChangeItTo;
+                    }
+
+                    this.items[i].quantity = this.quantityToChangeItTo;
+                }
+            }
+
+            this.nameOfItemToEdit = " ";
+        }
     },
 
     computed: {
