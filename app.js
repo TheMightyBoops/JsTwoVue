7049dbfa2e7abaf00f9f4a8b6f388a45d1153027
Lucas Nolting <lnolting22@gmail.com>
2019-03-05 22:59:25 -0600

    finally fixed a delete bug I noticed a long time ago, in a messy way

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-
-
 //Vue Components
 Vue.component('line-item', {
-    props: {item:Object, nameOfItemToEdit:String},
+    props: {item: Object, nameOfItemToEdit: String},
     methods: {
         toggleFavorite: function (event) {
             if (this.item.isFavorite) {
@@ -12,17 +10,27 @@ Vue.component('line-item', {
             }
         },
 
-        editThisItemByName: function(event) {
-          this.$parent.$parent.$parent.$parent.nameOfItemToEdit = this.item.name;
-        }
-    }
+        editThisItemByName: function (event) {
+            this.$parent.$parent.$parent.$parent.nameOfItemToEdit = this.item.name;
+        },
 
-    ,
+        customRemove() {
+            let itemsRef = this.$root.items;
+            for(let i = 0; i < itemsRef.length; ++i) {
+                itemsRef[i].id = i;
+            }
+
+            this.$root.items.splice(this.item.id,1);
+
+        }
+
+    },
+
     template: '<v-list><li>{{item.name}}<i class=material-icons v-if="item.isFavorite">favorite</i></li> ' +
     '<li><strong>Qty:</strong>{{item.quantity}}</li>' +
     '<v-btn color="success" v-on:click="toggleFavorite">Favorite</v-btn>' +
-    '<v-btn color="warning" v-on:click="editThisItemByName">edit</v-btn>'+
-    '<v-btn color="error" v-on:click="$emit(\'remove\')">Delete</v-btn></v-list>'
+    '<v-btn color="warning" v-on:click="editThisItemByName">edit</v-btn>' +
+    '<v-btn color="error" v-on:click="customRemove">Delete</v-btn></v-list>'
 });
 
 new Vue({
@@ -30,10 +38,10 @@ new Vue({
     el: "#app",
     data: {
         items: [
-            {id: 1, name: 'Furbies', quantity:20000 , isFavorite: true},
-            {id: 2, name: 'Tonnes of AAA batteries', quantity: 100, isFavorite: true},
-            {id: 3, name: 'Furby Sized Rail Guns', quantity: 200000, isFavorite: false},
-            {id: 4, name: 'Ai-chips', quantity:200000, isFavorite:false}
+            {id: 0, name: 'Furbies', quantity: 20000, isFavorite: true},
+            {id: 1, name: 'Tonnes of AAA batteries', quantity: 100, isFavorite: true},
+            {id: 2, name: 'Furby Sized Rail Guns', quantity: 200000, isFavorite: false},
+            {id: 3, name: 'Ai-chips', quantity: 200000, isFavorite: false}
         ],
 
         tempName: " ",
@@ -49,7 +57,7 @@ new Vue({
     methods: {
         addItem() {
 
-            if(this.tempName.trim() !== "") {
+            if (this.tempName.trim() !== "") {
                 for (let i = 0; i < this.items.length; ++i) {
 
                     if (this.items[i].name.trim() === this.tempName.trim()) {
@@ -88,9 +96,9 @@ new Vue({
         },
 
         editItem() {
-            for(let i=0; i < this.items.length; ++i) {
-                if(this.nameOfItemToEdit.trim() === this.items[i].name.trim()) {
-                    if(this.nameToChangeItTo.trim() !== '') {
+            for (let i = 0; i < this.items.length; ++i) {
+                if (this.nameOfItemToEdit.trim() === this.items[i].name.trim()) {
+                    if (this.nameToChangeItTo.trim() !== '') {
                         this.items[i].name = this.nameToChangeItTo;
                     }
 
@@ -108,7 +116,9 @@ new Vue({
 
     computed: {
         filteredItems() {
-            return this.items.filter(item => {return item.isFavorite === true})
+            return this.items.filter(item => {
+                return item.isFavorite === true
+            })
         }
     }
 });
